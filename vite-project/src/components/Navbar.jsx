fe975027b4669fbea4cfaeadfe1c4827ad94c1fe
Loua Ben Afia <loua12benafia@gmail.com>
2025-01-06 10:38:05 +0100

    Home page

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
         <p className="blue-gardient_text">AH</p>
     </NavLink>
     <nav className="flex text-lg gap-7 font-medium">
-    <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
           About
         </NavLink>
         <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
